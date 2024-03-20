3be528ad3e27996868084d7ed6fddcb10bd1d12a
Devu <devudevu10@gmail.com>
2024-03-20 22:13:05 +1100

    rotuing

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 import inquirer from "inquirer";
 import qr from "qr-image";
 import fs from "fs";
+import express from "express";
+const app = express();
+const port = 3000;
 
-inquirer
-  .prompt([
-    {
-      message: "Type in your URL: ",
-      name: "URL",
-    },
-  ])
-  .then((answers) => {
-    const url = answers.URL;
-    var qr_img = qr.image(url);
-    qr_img.pipe(fs.createWriteStream("qrimg.png"));
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-    } else {
-    }
-  });
+function testQr() {
+  return inquirer
+    .prompt([
+      {
+        message: "Type in your URL: ",
+        name: "URL",
+      },
+    ])
+    .then((answers) => {
+      const url = answers.URL;
+      var qr_img = qr.image(url);
+      qr_img.pipe(fs.createWriteStream("qrimg.png"));
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+      } else {
+      }
+    });
+}
+
+app.get("/", (req, res) => {
+  console.log(req.rawHeaders);
+  res.send("<h1>test Success</h1>");
+});
+
+app.get("/qr", (req, res) => {
+  console.log(req.header);
+  res.send("<h2>QR code</h2>");
+});
+
+app.listen(port, () => {
+  console.log("Server on port 3000");
+});
